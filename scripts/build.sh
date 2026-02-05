77a7da56e4d31e0f40fe6041d8ff2b431b9735f1
Alessandro Vito <alessandro.vito84@gmail.com>
2026-02-05 10:37:48 +0000

    add action for building and testing on push

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+##
+# Build the WASM package for nodejs
+##
+
+## Bash settings
+
+# Fail fast
+# set -e
+# Enable debug mode
+# set -x
+
+wasm-pack build --target nodejs --dev
+cp ./types/zcash_decrypt.d.ts pkg
+rm ./pkg/package.json
